5c499aa847ac964f7e7eb8289e0dbbfa5c9a8411
Tomás Di Domenico <tdido@tdido.com.ar>
2019-10-25 17:03:44 +0200

    Fixed missing index in sample processing script.

--- scripts/analyse_sample.sh
+++ scripts/analyse_sample.sh
@@ -10,6 +10,9 @@ then
     mkdir -p out/cutadapt
     cutadapt -m 20 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT -o out/cutadapt/${sampleid}_1.trimmed.fastq.gz -p out/cutadapt/${sampleid}_2.trimmed.fastq.gz data/${sampleid}_1.fastq.gz data/${sampleid}_2.fastq.gz > log/cutadapt/${sampleid}.log
     echo
+    echo "Running STAR index..."
+    STAR --runThreadN 4 --runMode genomeGenerate --genomeDir res/genome/star_index/ --genomeFastaFiles res/genome/ecoli.fasta --genomeSAindexNbases 9
+    echo
     echo "Running STAR alignment..."
     mkdir -p out/star/${sampleid}
     STAR --runThreadN 4 --genomeDir res/genome/star_index/ --readFilesIn out/cutadapt/${sampleid}_1.trimmed.fastq.gz out/cutadapt/${sampleid}_2.trimmed.fastq.gz --readFilesCommand zcat --outFileNamePrefix out/star/${sampleid}/
